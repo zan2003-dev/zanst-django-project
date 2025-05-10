4ad00e5c783ed109587393e1e2a006c4dd2d8bd0
zan2003-dev <zanst1250@gmail.com>
2025-05-10 22:07:17 +0300

    Delete multipage_project directory

--- multipage_project/multipage_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),  # This includes all pages URLs at root
-]
